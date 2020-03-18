54132157d89c690ddc0d74871fcaeeaac1ea20a3
SamTyurenkov <sam.tyurenkov@gmail.com>
2020-03-18 22:13:49 +0300

    fixed a bug

--- time2str.php
+++ time2str.php
@@ -3,6 +3,7 @@
 function format_duration(int $seconds):string {
 
 $string = '';
+$array = array();
 
 $mint = 2147483647; //max integer value on 32bit php ~~~68 years;
 $year = 31536000;   //seconds in year with 365 days
@@ -31,50 +32,54 @@ $string .= $years . ' лет, ';
 }
 
 if ($months % 10 == 1 && $months != 11) {
-$string .= $months . ' месяц, ';
+$array[] = $months . ' месяц';
 } elseif (in_array($months % 10,$array1) && !in_array($months,$array2)) {
-$string .= $months . ' месяца, ';	
+$array[] = $months . ' месяца';	
 } elseif ($months != 0) {
-$string .= $months . ' месяцев, ';		
+$array[] = $months . ' месяцев';		
 }
 
 if ($days % 10 == 1 && $days != 11) {
-$string .= $days . ' день, ';
+$array[] = $days . ' день';
 } elseif (in_array($days % 10,$array1) && !in_array($days,$array2)) {
-$string .= $days . ' дня, ';	
+$array[] = $days . ' дня';	
 } elseif ($days != 0) {
-$string .= $days . ' дней, ';		
+$array[] = $days . ' дней';		
 }
 
 if ($hours % 10 == 1 && $hours != 11) {
-$string .= $hours . ' час, ';
+$array[] = $hours . ' час';
 } elseif (in_array($hours % 10,$array1) && !in_array($hours,$array2)) {
-$string .= $hours . ' часа, ';	
+$array[] = $hours . ' часа';	
 } elseif ($hours != 0) {
-$string .= $hours . ' часов, ';		
+$array[] = $hours . ' часов';		
 }
 
 if ($minutes % 10 == 1 && $minutes != 11) {
-$string .= $minutes . ' минута, ';
+$array[] = $minutes . ' минута';
 } elseif (in_array($minutes % 10,$array1) && !in_array($minutes,$array2)) {
-$string .= $minutes . ' минуты, ';	
+$array[] = $minutes . ' минуты';	
 } elseif ($minutes != 0) {
-$string .= $minutes . ' минут, ';		
+$array[] = $minutes . ' минут';		
 }
 
 if ($seconds % 10 == 1 && $seconds != 11) {
-$string .= $seconds . ' секунда';
+$array[] = $seconds . ' секунда';
 } elseif (in_array($seconds % 10,$array1) && !in_array($seconds,$array2)) {
-$string .= $seconds . ' секунды';	
+$array[] = $seconds . ' секунды';	
 } elseif ($seconds != 0) {
-$string .= $seconds . ' секунд';		
-} elseif ($string == '') {
-$string .= 'только что';	
+$array[] = $seconds . ' секунд';		
+} elseif (empty($array)) {
+$array[] = 'только что';	
 }
 
-//replace last comma with regex and remove it if its the end of the string
-$string = preg_replace('/,(?!.*,)/', ' и ', $string);
-$string = preg_replace('/ и $/', '', $string);
+$count = count($array);
+
+switch ($count) {
+	case 1: $string = implode ('', $array); break;
+	case 2: $string = implode (' и ', $array); break;
+	default: $string = implode (', ', $array); $string = preg_replace('/,(?!.*,)/', ' и ', $string); break;
+}
 
 return $string;
 }
